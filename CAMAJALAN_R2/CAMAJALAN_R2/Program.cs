059b374fb0fd5f9b9d16477b676405a0cea4d7c6
Aivy Camajalan <camajalanaivy63@gmail.com>
2026-01-15 16:40:16 +0800

    Add files via upload

---
+++ CAMAJALAN_R2/CAMAJALAN_R2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMAJALAN_R2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create instances of each type of shape and a parameterized constructor
+            Shape circle = new Circle("Yellow", 9);
+            Shape rectangle = new Rectangle("Black", 2, 3);
+
+            //Display the Area of the Circle
+            circle.DisplayInfo();
+            Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+
+            //Display the Area of the rectangle
+            rectangle.DisplayInfo();
+            Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CAMAJALAN_R2/CAMAJALAN_R2/Shape.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMAJALAN_R2
+{
+    class Shape
+    {
+        //Private Fields Declaration
+        private string Color { get; set; }
+
+        //Constructor
+        public Shape(string color)
+        {
+            Color = color;
+        }
+
+        //virtual method 
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+
+        //Method to display shoes info
+        public void DisplayInfo() 
+        {
+            Console.WriteLine($"\nType of Shape: {this.GetType().Name}  \nColor: {Color} ");
+        }
+
+    }
+
+    class Circle : Shape //Derived Class1
+    {
+        //Private Fields Declaration
+        private double Radius { get; set; }
+
+        //Constructor
+        public Circle(string color, double radius) : base(color)
+        {
+            Radius = radius;
+        }
+        //override method
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        //Private Fields Declaration
+        private double Width { get; set; }
+        private double Height { get; set; }
+
+        //Constructor
+        public Rectangle(string color, double width, double height) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+        // method override
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+
+    }
+}
